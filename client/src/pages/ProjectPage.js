0da0098f1bc751efd1375e98f42dd20b270e8715
Yu-Ting Lin <132069361+NICKLIN13@users.noreply.github.com>
2024-11-26 17:25:59 +0800

    Added some new projects to project page

--- client/src/pages/ProjectPage.js
+++ client/src/pages/ProjectPage.js
@@ -19,18 +19,61 @@ function ProjectPage() {
         <div className='container'>
           <div className="card">
             <div className='card-image'>
+              <img src="nicklin13_portfolio/Project-Images/terrain.png" alt="Hangman Game" style={{ width: '286px', height: '256px', objectFit: 'cover', objectPosition: 'center top' }} />
+            </div>
+            <div className="card-body">
+              <h4>3D terrain generator</h4>
+              <div className='text-start fs-6' style={{ marginTop: '9px' }}>
+                <span className="badge bg-secondary d-inline-block">JavaScript</span>
+                <span className="badge bg-secondary d-inline-block" style={{ marginLeft: '2px' }}>WebGL</span>
+              </div>
+
+              <CardText style={{ marginTop: '15px' }}>
+                <ul className="fs-6 text-start">
+                  <li>Implemented a CPU-GPU pipeline</li>
+                  <li>Implemented a 3D realistic terrain with Blinn-Phong shading</li>
+                  <li>Interactive "drive" and "fly" modes, and erosion effects</li>
+                </ul>
+              </CardText>
+              <Button className='custom-button' href="https://github.com/NICKLIN13/Personal-Side-Works/tree/main/hangman-typescript" class="btn btn-primary" target="_blank">GitHub</Button>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className='card-image'>
+              <img src="nicklin13_portfolio/Project-Images/textures.png" alt="Rasterizer example img" style={{ background:'#778899', width: '256px', height: '256px', objectFit: 'cover', objectPosition: 'center top' }} />
+            </div>
+            <div className="card-body">
+              <h4>Rasterizer</h4>
+              <div className='text-start fs-6' style={{ marginTop: '9px' }}>
+                <span className="badge bg-secondary d-inline-block">Python</span>
+                <span className="badge bg-secondary d-inline-block" style={{ marginLeft: '2px' }}>DDA Algorithm</span>
+              </div>
+
+              <CardText style={{ marginTop: '15px' }}>
+                <ul className="fs-6 text-start">
+                  <li>Implemented a rasterization pipeline with DDA algorithm</li>
+                  <li>Rendered png images by reading the txt files</li>
+                </ul>
+              </CardText>
+              <Button className='custom-button' href="https://github.com/NICKLIN13/Personal-Side-Works/tree/main/python-flask-google-login" class="btn btn-primary" target="_blank">GitHub</Button>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className='card-image'>
               <img src="nicklin13_portfolio/Project-Images/IMAGE_To-do-list.png" alt="TodoList App" style={{ width: '286px', height: '256px', objectFit: 'cover', objectPosition: 'center top' }} />
             </div>
             <div className="card-body">
               <h4>Todo List</h4>
               <div className='text-start fs-6' style={{ marginTop: '9px' }}>
-                <span className="badge bg-secondary d-inline-block">NodeJS</span>
-                <span className="badge bg-secondary d-inline-block" style={{ marginLeft: '2px'}}>JavaScript(React)</span>
-                <span className="badge bg-secondary d-inline-block">MongoDB</span>
-                <span className="badge bg-secondary d-inline-block" style={{ marginLeft: '2px'}}>Deployed(CI/CD)</span>
+                <span className="badge bg-secondary d-inline-block">Node.js</span>
+                <span className="badge bg-secondary d-inline-block" style={{ marginLeft: '2px' }}>React.js</span>
+                <span className="badge bg-secondary d-inline-block" style={{ marginLeft: '2px' }}>MongoDB</span>
+                <span className="badge bg-secondary d-inline-block">Deployed(CI/CD)</span>
               </div>
 
-              <CardText style={{ marginTop: '15px'}}>
+              <CardText style={{ marginTop: '15px' }}>
                 <ul className="fs-6 text-start">
                   <li>Utilized React.js & Express.js to do the full stack work</li>
                   <li>Integrated MongoDB as a back-end database with RESTful Api</li>
@@ -40,53 +83,6 @@ function ProjectPage() {
               <Button className='custom-button' href="https://lin-todolist.onrender.com" class="btn btn-primary" target="_blank">Demo</Button>
             </div>
           </div>
-
-          <div className="card">
-            <div className='card-image'>
-              <img src="nicklin13_portfolio/Project-Images/Hangman.png" alt="Hangman Game" style={{ width: '286px', height: '256px', objectFit: 'cover', objectPosition: 'center top' }} />
-            </div>
-            <div className="card-body">
-              <h4>Hangman</h4>
-              <div className='text-start fs-6' style={{ marginTop: '9px' }}>
-                <span className="badge bg-secondary d-inline-block">TypeScript(React)</span>
-              </div>
-
-              <CardText style={{ marginTop: '15px'}}>
-                <ul className="fs-6 text-start">
-                  <li>Developed Hangman game using TypeScript</li>
-                  <li>Kept users from entering repeated letters</li>
-                  <li>Generated a touch keyboard</li>
-                </ul>
-              </CardText>
-              <Button className='custom-button' href="https://github.com/NICKLIN13/Personal-Side-Works/tree/main/hangman-typescript" class="btn btn-primary" target="_blank">GitHub</Button>
-            </div>
-          </div>
-
-          <div className="card">
-            <div className='card-image'>
-              <img src="nicklin13_portfolio/Project-Images/Login-with-Google.png" alt="Login with Google" style={{ width: '286px', height: '256px', objectFit: 'cover', objectPosition: 'center top' }} />
-            </div>
-            <div className="card-body">
-              <h4>Login with Google</h4>
-              <div className='text-start fs-6' style={{ marginTop: '9px' }}>
-                <span className="badge bg-secondary d-inline-block">Python(Flask)</span>
-                <span className="badge bg-secondary d-inline-block" style={{ marginLeft: '2px'}}>OAuth 2.0</span>
-              </div>
-
-
-
-              <CardText style={{ marginTop: '15px'}}>
-                <ul className="fs-6 text-start">
-                  <li>Integrated Google OAuth 2.0 API</li>
-                  <li>Prevented CSRF attacks with id_token</li>
-                  <li>Protected specific URLs with decorator</li>
-                </ul>
-              </CardText>
-              <Button className='custom-button' href="https://github.com/NICKLIN13/Personal-Side-Works/tree/main/python-flask-google-login" class="btn btn-primary" target="_blank">GitHub</Button>
-            </div>
-          </div>
-
-
         </div>
 
       </header>
